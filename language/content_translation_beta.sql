1cd18e26c67c161c276665ac24dda55507f09383
milimetric <dandreescu@wikimedia.org>
2015-03-11 08:36:43 -0400

    Fix select order
    These sql scripts are executed daily and append to the same TSV.  For
    that reason, it's important that the columns selected don't change
    order, otherwise the original heading row can not be updated to make
    sense of the data rows underneath.
    
    Bug: T92236
    Change-Id: I744f03d5b3ceedda76c60cc1be347a90aee44814

--- language/content_translation_beta.sql
+++ language/content_translation_beta.sql
@@ -4,15 +4,15 @@ select current_date() as date,
 	eowiki,
 	eswiki,
 	idwiki,
-	kywiki,
 	mswiki,
 	nnwiki,
 	nowiki,
-	pawiki,
 	ptwiki,
 	svwiki,
 	minwiki,
-	uzwiki
+	uzwiki,
+	kywiki,
+	pawiki
 	from
 ( select count(*) as cawiki from cawiki.user_properties where up_property = 'cx' and up_value = 1 ) ca
 left join
@@ -24,15 +24,11 @@ left join
 left join
 ( select count(*) as idwiki from idwiki.user_properties where up_property = 'cx' and up_value = 1 ) id on 1=1
 left join
-( select count(*) as kywiki from kywiki.user_properties where up_property = 'cx' and up_value = 1 ) ky on 1=1
-left join
 ( select count(*) as mswiki from mswiki.user_properties where up_property = 'cx' and up_value = 1 ) ms on 1=1
 left join
 ( select count(*) as nnwiki from nnwiki.user_properties where up_property = 'cx' and up_value = 1 ) nn on 1=1
 left join
 ( select count(*) as nowiki from nowiki.user_properties where up_property = 'cx' and up_value = 1 ) no on 1=1
-left join
-( select count(*) as pawiki from pawiki.user_properties where up_property = 'cx' and up_value = 1 ) pa on 1=1
 left join
 ( select count(*) as ptwiki from ptwiki.user_properties where up_property = 'cx' and up_value = 1 ) pt on 1=1
 left join
@@ -41,4 +37,8 @@ left join
 ( select count(*) as minwiki from minwiki.user_properties where up_property = 'cx' and up_value = 1 ) min on 1=1
 left join
 ( select count(*) as uzwiki from uzwiki.user_properties where up_property = 'cx' and up_value = 1 ) uz on 1=1
+left join
+( select count(*) as kywiki from kywiki.user_properties where up_property = 'cx' and up_value = 1 ) ky on 1=1
+left join
+( select count(*) as pawiki from pawiki.user_properties where up_property = 'cx' and up_value = 1 ) pa on 1=1
 ;
