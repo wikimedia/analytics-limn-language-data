f9addf0eb1ea14b6974ce0c6fc68488baa9ff878
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2019-03-25 12:12:47 -0700

    Clean up the dashboard labels
    * Remove unnecessary and invalid engines
    * Relabel "scratch" as 'No machine translation'
    
    Bug: T210135
    
    Change-Id: I8c14703abf1948b014344ce260615192395aa737

--- mt_engines/mt_engines.sql
+++ mt_engines/mt_engines.sql
@@ -1,11 +1,22 @@
 select
   date('{from_timestamp}') as date,
-  cxc_origin as engine,
+  case cxc_origin
+    when 'scratch' then 'No machine translation'
+    else cxc_origin
+    end
+    as engine,
   count(distinct(cxc_translation_id)) as translations
 from
   cx_corpora
 where
   cxc_timestamp between '{from_timestamp}' and '{to_timestamp}' and
-  cxc_origin not in ('disable-mt', 'no-mt', 'user', 'source', 'source-mt')
+  cxc_origin not in (
+    'disable-mt',
+    'no-mt',
+    'source',
+    'TestClient',
+    'source-mt',
+    'user'
+  )
 group by
   cxc_origin;
